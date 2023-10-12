a4f4301d88ea1c46e335e203cae57e09b132e7f5
yeekrim <kr031106@naver.com>
2023-10-12 12:04:07 +0900

    6주차 중...

--- 실습_6주차.py
+++ 실습_6주차.py
@@ -1,5 +1,23 @@
+#1
 ary = [1,10,5,8,7,6,4,3,2,9]
 
+def max_heapify(array):
+    last = len(array) // 2 - 1
+    for current in range(last, -1, -1):
+        while current <= last:
+            child = current * 2 + 1
+            sibling = child + 1
+            if sibling < len(array) and array[child] < array[sibling]:
+                child = sibling
+            if array[current] < array[child]:
+                array[current], array[child] = array[child], array[current]
+                current = child
+            else:
+                break
+
+    return array
+
+#2
 def heap_sort(array):
     n = len(array)
 
@@ -23,26 +41,18 @@ def heap_sort(array):
             last = i
     return array
 
-
-def max_heapify(array):
-    last = len(array) // 2 - 1
-    for current in range(last, -1, -1):
-        while current <= last:
-            child = current * 2 + 1
-            sibling = child + 1
-            if sibling < len(array) and array[child] < array[sibling]:
-                child = sibling
-            if array[current] < array[child]:
-                array[current], array[child] = array[child], array[current]
-                current = child
-            else:
-                break
-
-    return array
-
+#3
 print('s = ', end='')
 s = list(map(int,input().split()))
 
 heap_sort(s)
 
 print("\""f'{s[0]}' ' ' f'{s[-1]}'"\"")
+
+#4
+
+
+#5
+
+#6
+
